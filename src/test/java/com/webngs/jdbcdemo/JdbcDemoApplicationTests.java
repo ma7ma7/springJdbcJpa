92215539a399e4fa3f6a61a3240accfef3e0493a
Amane Mahmoud <mahmoudamane5@gmail.com>
2020-04-17 19:48:19 +0100

    Enable h2 console

---
+++ src/test/java/com/webngs/jdbcdemo/JdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webngs.jdbcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
